245f2ce0a221bebfda108cfda694ef7cf6f18f3b
Subodh Ingole <subodh.ingole57@gmail.com>
2024-04-01 19:53:21 +0530

    added "/" to publicRoutes

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,40 +1,32 @@
-import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
-import Image from "next/image";
 import Link from "next/link";
 
 function Topbar() {
   return (
-    <nav className='topbar'>
-      <Link href='/' className='flex items-center gap-4'>
-        <Image src='/logo.svg' alt='logo' width={28} height={28} />
-        <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "1rem",
+        backgroundColor: "black",
+        color: "white",
+      }}
+    >
+      <Link href="/">
+        <a style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+          {/* You might replace this with an img tag if you don't want to use next/image */}
+          {/* <img src="/logo.svg" alt="logo" width={28} height={28} /> */}
+          <p style={{ fontSize: "1.25rem", fontWeight: "bold" }}>Threads</p>
+        </a>
       </Link>
 
-      <div className='flex items-center gap-1'>
-        <div className='block md:hidden'>
-          <SignedIn>
-            <SignOutButton>
-              <div className='flex cursor-pointer'>
-                <Image
-                  src='/assets/logout.svg'
-                  alt='logout'
-                  width={24}
-                  height={24}
-                />
-              </div>
-            </SignOutButton>
-          </SignedIn>
-        </div>
-
-        <OrganizationSwitcher
-          appearance={{
-            baseTheme: dark,
-            elements: {
-              organizationSwitcherTrigger: "py-2 px-4",
-            },
-          }}
-        />
+      <div>
+        <Link href="/profile">
+          <a style={{ marginRight: "1rem" }}>Profile</a>
+        </Link>
+        <Link href="/logout">
+          <a>Logout</a>
+        </Link>
       </div>
     </nav>
   );
