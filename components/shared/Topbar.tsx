5361f74c652a38fc2378f0bc2ba201bc93245014
Subodh Ingole <subodh.ingole57@gmail.com>
2024-04-02 21:39:10 +0530

    Topbar Link tag changes

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -12,21 +12,18 @@ function Topbar() {
         color: "white",
       }}
     >
-      <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
-          {/* You might replace this with an img tag if you don't want to use next/image */}
-          {/* <img src="/logo.svg" alt="logo" width={28} height={28} /> */}
-          <p style={{ fontSize: "1.25rem", fontWeight: "bold" }}>Threads</p>
-        </a>
+      <Link
+        style={{ display: "flex", alignItems: "center", gap: "1rem" }}
+        href="/"
+      >
+        Threads
       </Link>
 
       <div>
-        <Link href="/profile">
-          <a style={{ marginRight: "1rem" }}>Profile</a>
+        <Link style={{ marginRight: "1rem" }} href="/profile">
+          Profile
         </Link>
-        <Link href="/logout">
-          <a>Logout</a>
-        </Link>
+        <Link href="/logout">Logout</Link>
       </div>
     </nav>
   );
